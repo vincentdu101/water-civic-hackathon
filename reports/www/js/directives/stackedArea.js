964459dcca48e42ed6db595b1ffb4b4605c498ab
Vincent Du <vincentdu101@gmail.com>
2015-06-07 13:33:02 -0700

    addressed categories

--- reports/www/js/directives/stackedArea.js
+++ reports/www/js/directives/stackedArea.js
@@ -7,18 +7,21 @@ app.directive('stackedArea', [
     templateUrl: 'templates/shared/stackedArea.html',
     controller: function($scope) {
       var allData = [];
+      var sample = [];
+      var orderedCategories = [];
+      var categories = [];
+      var allCategories = {};
 //       function GetSomeDeferredStuff() {
 //         var deferreds = [];
 //         var files = ["ANT.,csv",  "BUC.csv",  "CCH.csv",  "CMN.csv",  "DNN.csv",  "ENG.csv",  "FRD.csv",  "ICH.csv",  "ISB.csv",  "LON.csv",  "NHG.csv",  "PAR.csv",  "PYM.csv",  "SNL.csv",  "TUL.csv",  "WRS.csv",
 // ",BLB.csv",  "BUL.csv",  "CHV.csv",  "COY.csv",  "DNP.csv",  "EXC.csv",  "HID.csv",  "INP.csv",  "KES.csv",  "MIL.csv",  "NML.csv",  "PNF.csv",  "SCC.csv",  "STP.csv",  "UNV.csv",
 // ",BRD.csv",  "CAS.csv",  "CLE.csv",  "DAV.csv",  "DON.csv",  "FOL.csv",  "HTH.csv",  "INV.csv",  "LEW.csv",  "NAT.csv",  "ORO.csv",  "PRR.csv",  "SHA.csv",  "TRM.csv",  "WHI.csv"]; 
-
 //         var i = 1;
 //         for (i = 1; i <= 10; i++) {
 //             var count = i;
 
 //             deferreds.push(
-//               d3.csv('js/data/' + files[i], function(data){
+//               d3.csv('js/data/' + files"[i]", function(data){
 //                 allData.push(data);
 //                 $scope.$broadcast('')
 //               })
@@ -27,180 +30,88 @@ app.directive('stackedArea', [
         
 //         return deferreds;
 //       }
+      function swap(index1, index2) {
+        var temp = categories[index1];
+        categories[index1] = categories[index2];
+        categories[index2] = temp;
+      }
 
-      var chart = AmCharts.makeChart("chartdiv", {
-          "type": "serial",
-          "theme": "light",
-          "marginRight":30,
-          "path": "http://www.amcharts.com/lib/3/",
-          "legend": {
-              "equalWidths": false,
-              "periodValueText": "total: [[value.sum]]",
-              "position": "top",
-              "valueAlign": "left",
-              "valueWidth": 100
-          },
-          "dataProvider": [{
-              "year": "06/07/2003",
-              "cars": 1587,
-              "motorcycles": 650,
-              "bicycles": 121
-          }, {
-              "year": "06/08/2003",
-              "cars": 1567,
-              "motorcycles": 683,
-              "bicycles": 146
-          }, {
-              "year": "06/09/2003",
-              "cars": 1617,
-              "motorcycles": 691,
-              "bicycles": 138
-          }, {
-              "year": "06/10/2003",
-              "cars": 1630,
-              "motorcycles": 642,
-              "bicycles": 127
-          }, {
-              "year": "06/11/2003",
-              "cars": 1660,
-              "motorcycles": 699,
-              "bicycles": 105
-          }, {
-              "year": "06/12/2003",
-              "cars": 1683,
-              "motorcycles": 721,
-              "bicycles": 109
-          }, {
-              "year": "06/13/2003",
-              "cars": 1691,
-              "motorcycles": 737,
-              "bicycles": 112
-          }, {
-              "year": "06/14/2003",
-              "cars": 1298,
-              "motorcycles": 680,
-              "bicycles": 101
-          }, {
-              "year": 2002,
-              "cars": 1275,
-              "motorcycles": 664,
-              "bicycles": 97
-          }, {
-              "year": "06/21/2003",
-              "cars": 1246,
-              "motorcycles": 648,
-              "bicycles": 93
-          }, {
-              "year": "06/28/2003",
-              "cars": 1318,
-              "motorcycles": 697,
-              "bicycles": 111
-          }, {
-              "year": "06/30/2004",
-              "cars": 1213,
-              "motorcycles": 633,
-              "bicycles": 87
-          }, {
-              "year": "07/30/2004",
-              "cars": 1199,
-              "motorcycles": 621,
-              "bicycles": 79
-          }, {
-              "year": "06/30/2008",
-              "cars": 1110,
-              "motorcycles": 210,
-              "bicycles": 81
-          }, {
-              "year": "06/30/2009",
-              "cars": 1165,
-              "motorcycles": 232,
-              "bicycles": 75
-          }, {
-              "year": "06/30/2012",
-              "cars": 1145,
-              "motorcycles": 219,
-              "bicycles": 88
-          }, {
-              "year": "06/30/2013",
-              "cars": 1163,
-              "motorcycles": 201,
-              "bicycles": 82
-          }, {
-              "year": "06/30/2014",
-              "cars": 1180,
-              "motorcycles": 285,
-              "bicycles": 87
-          }, {
-              "year": '06/30/2015',
-              "cars": 1159,
-              "motorcycles": 277,
-              "bicycles": 71
-          }],
-          "valueAxes": [{
-              "stackType": "regular",
-              "gridAlpha": 0.07,
-              "position": "left",
-              "title": "Traffic incidents"
-          }],
-          "graphs": [{
-              "balloonText": "<img src='http://www.amcharts.com/lib/3/images/car.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
-              "fillAlphas": 0.6,
-              "hidden": true,
-              "lineAlpha": 0.4,
-              "title": "Cars",
-              "valueField": "cars"
-          }, {
-              "balloonText": "<img src='http://www.amcharts.com/lib/3/images/motorcycle.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
-              "fillAlphas": 0.6,
-              "lineAlpha": 0.4,
-              "title": "Motorcycles",
-              "valueField": "motorcycles"
-          }, {
-              "balloonText": "<img src='http://www.amcharts.com/lib/3/images/bicycle.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'><b>[[value]]</b></span>",
-              "fillAlphas": 0.6,
-              "lineAlpha": 0.4,
-              "title": "Bicycles",
-              "valueField": "bicycles"
-          }],
-          "plotAreaBorderAlpha": 0,
-          "marginTop": 10,
-          "marginLeft": 0,
-          "marginBottom": 0,
-          "chartScrollbar": {},
-          "chartCursor": {
-              "cursorAlpha": 0
-          },
-          "categoryField": "year",
-          "categoryAxis": {
-              "startOnAxis": true,
-              "axisColor": "#DADADA",
-              "gridAlpha": 0.07,
-              "guides": [{
-                  category: "2001",
-                  toCategory: "2003",
-                  lineColor: "#CC0000",
-                  lineAlpha: 1,
-                  fillAlpha: 0.2,
-                  fillColor: "#CC0000",
-                  dashLength: 2,
-                  inside: true,
-                  labelRotation: 90,
-                  label: "fines for speeding increased"
-              }, {
-                  category: "2007",
-                  lineColor: "#CC0000",
-                  lineAlpha: 1,
-                  dashLength: 2,
-                  inside: true,
-                  labelRotation: 90,
-                  label: "motorcycle fee introduced"
-              }]
-          },
-          "export": {
-            "enabled": true
-           }
-      });
+      function sortByLargest(dataStore) {
+        var num_elements = categories.length; 
+        var temp;
+        // for(var outer = num_elements; outer >= 2; --outer){
+        //   for(var inner = 0; inner <= outer -1; ++inner){
+        //     if(firstRow[categories[inner]] > firstRow[categories[inner + 1]]){
+        //       swap(inner, inner + 1);
+        //     }
+        //   }
+        // }
+        for (var i = 0; i < categories.length; i++) {
+          orderedCategories.push(allCategories[categories[i]]);
+        }
+      }
 
+      function genChart() {
+        console.log(allData);
+        var chart = AmCharts.makeChart("chartdiv", {
+            "type": "serial",
+            "theme": "light",
+            "marginRight":30,
+            "path": "http://www.amcharts.com/lib/3/",
+            "legend": {
+                "equalWidths": false,
+                "periodValueText": "total: [[value.sum]]",
+                "position": "top",
+                "valueAlign": "left",
+                "valueWidth": 100
+            },
+            "dataProvider": allData,
+            "valueAxes": [{
+                "stackType": "regular",
+                "gridAlpha": 0.07,
+                "position": "left",
+                "title": "Water Reservoir Levels"
+            }],
+            "graphs": orderedCategories,
+            "plotAreaBorderAlpha": 0,
+            "marginTop": 10,
+            "marginLeft": 0,
+            "marginBottom": 0,
+            "chartScrollbar": {},
+            "chartCursor": {
+                "cursorAlpha": 0
+            },
+            "categoryField": "date",
+            "categoryAxis": {
+                "startOnAxis": true,
+                "axisColor": "#DADADA",
+                "gridAlpha": 0.07,
+                "guides": [{
+                    category: "2001",
+                    toCategory: "2003",
+                    lineColor: "#CC0000",
+                    lineAlpha: 1,
+                    fillAlpha: 0.2,
+                    fillColor: "#CC0000",
+                    dashLength: 2,
+                    inside: true,
+                    labelRotation: 90,
+                    label: "fines for speeding increased"
+                }, {
+                    category: "2007",
+                    lineColor: "#CC0000",
+                    lineAlpha: 1,
+                    dashLength: 2,
+                    inside: true,
+                    labelRotation: 90,
+                    label: "motorcycle fee introduced"
+                }]
+            },
+            "export": {
+              "enabled": true
+             }
+        });
+      }
       // $(function() {
           // $("a").click(function() {
               // var deferreds = GetSomeDeferredStuff();
@@ -216,14 +127,28 @@ app.directive('stackedArea', [
 ",BRD.csv",  "CAS.csv",  "CLE.csv",  "DAV.csv",  "DON.csv",  "FOL.csv",  "HTH.csv",  "INV.csv",  "LEW.csv",  "NAT.csv",  "ORO.csv",  "PRR.csv",  "SHA.csv",  "TRM.csv",  "WHI.csv"]; 
 
       var i = 1;
-      for (i = 1; i <= 10; i++) {
-          var count = i;
+      // for (i = 1; i <= 10; i++) {
+      //     var count = i;
 
-          deferreds.push(
-            d3.csv('js/data/' + files[i], function(data){
-              console.log(data);
-            })
-          );
+      //     deferreds.push(
+      //       d3.csv('js/data/' + files[i], function(data){
+      //         allData = data;
+      //         genChart();
+      //       })
+      //     );
+      // }
+      queue()
+        .defer(d3.csv, 'js/data/all.csv')
+        .defer(d3.json, 'js/data/categories.json')
+        .await(init);
+      
+      function init(error, data, headers){
+        allData = data;
+        firstRow = allData[0];
+        categories = Object.keys(headers);
+        allCategories = headers;
+        sortByLargest();
+        genChart();
       }
     }
   }
